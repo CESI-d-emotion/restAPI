244462fc6afd2be8af40739be01fc43b257da4d3
lbAntoine <antoine.lebras@gmail.com>
2024-04-11 13:43:13 +0200

    fix static

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,28 +1,23 @@
-import {Repository} from "typeorm";
-import {User} from "../entity/User";
-import {AppDataSource} from "../data-source";
-import * as cache from "memory-cache"
-import {log} from "../helpers/logger.helper";
+import { Repository } from 'typeorm'
+import { User } from '../entity/User'
+import { AppDataSource } from '../data-source'
+import * as cache from 'memory-cache'
+import { log } from '../helpers/logger.helper'
 
 export class UserService {
-    private userRepo: Repository<User>
+  static userRepo: Repository<User> = AppDataSource.getRepository(User)
 
-    constructor() {
-        this.userRepo = AppDataSource.getRepository(User)
+  static async getUsers() {
+    const data = cache.get('data')
+    if (data) {
+      log.info('Serving from cache')
+      return data
+    } else {
+      log.info('Serving from db')
+      const users = await this.userRepo.find()
+
+      cache.put('data', users, 6000)
+      return users
     }
-
-    static async getUsers() {
-        const data = cache.get('data')
-        if (data) {
-            log.info('Serving from cache')
-            return data
-        } else {
-            log.info('Serving from db')
-            const userRepo = AppDataSource.getRepository(User)
-            const users = await userRepo.find()
-
-            cache.put('data', users, 6000)
-            return users
-        }
-    }
+  }
 }
